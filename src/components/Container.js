641cf5e31f68a5a84d7b2babbef0b3de0d4aa06f
Jonathan Lattanzi <jonathanlattanzi@gmail.com>
2019-01-14 17:23:14 -0600

    Added delete and new functions

--- src/components/Container.js
+++ src/components/Container.js
@@ -10,10 +10,27 @@ class Container extends Component {
         this.state = {
             students: students,
             currentIndex: 0,
+            showInput: false,
+            newForm: {
+                name: '',
+                from: '',
+                funFact: '',
+                cityOrCountry: '',
+                indoorsOrOutdoors: '',
+                travel: '',
+                food: '',
+                dogOrCat: '',
+            },
         }
 
         this.handlePrevClick = this.handlePrevClick.bind(this)
         this.handleNextClick = this.handleNextClick.bind(this)
+        this.deleteStudent = this.deleteStudent.bind(this)
+        this.showForm = this.showForm.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+        // this.addStudent = this.addStudent.bind(this)
+        // this.editStudent = this.editStudent.bind(this)
     }
 
     handlePrevClick() {
@@ -34,6 +51,47 @@ class Container extends Component {
         }
     }
 
+    handleChange(e) {
+        let obj = {
+            ...this.state.newForm,
+            [e.target.name]: e.target.value,
+        }
+
+        this.setState({
+            newForm: obj,
+        })
+    }
+
+    handleSubmit(e) {
+        e.preventDefault()
+        console.log(this.state.newForm)
+
+        let arr = this.state.students.slice()
+        arr.push(this.state.newForm)
+
+        this.setState({ students: arr })
+    }
+
+    editStudent(index) {
+        // edit lol
+        
+    }
+
+    deleteStudent() {
+        if (this.state.students.length > 1) {
+            let arr = [...this.state.students]
+            arr.splice(this.state.currentIndex, 1)
+
+            this.setState({ students: arr })
+        } else {
+            return false
+        }
+    }
+
+    showForm() {
+        this.setState({ showInput: !this.state.showInput })
+    }
+
     render() {
         return (
             <div className={styles.Container}>
@@ -44,8 +102,67 @@ class Container extends Component {
                 />
                 <div className={styles.Controls}>
                     <button onClick={this.handlePrevClick}>&lt; prev</button>
+                    {/* <button onClick={this.showForm}>edit</button> */}
+                    <button onClick={this.deleteStudent}>delete</button>
+                    <button onClick={this.showForm}>new</button>
                     <button onClick={this.handleNextClick}>next &gt;</button>
                 </div>
+
+                <form
+                    className={
+                        this.state.showInput ? styles.InputForm : styles.Hidden
+                    }
+                    onSubmit={(e) => this.handleSubmit(e)}>
+                    <input
+                        type='text'
+                        name='name'
+                        placeholder='name'
+                        onChange={(e) => this.handleChange(e)}
+                    />
+                    <input
+                        type='text'
+                        name='from'
+                        placeholder='from'
+                        onChange={(e) => this.handleChange(e)}
+                    />
+                    <input
+                        type='text'
+                        name='funFact'
+                        placeholder='fun fact'
+                        onChange={(e) => this.handleChange(e)}
+                    />
+                    <input
+                        type='text'
+                        name='cityOrCountry'
+                        placeholder='city or country'
+                        onChange={(e) => this.handleChange(e)}
+                    />
+                    <input
+                        type='text'
+                        name='indoorsOrOutdoors'
+                        placeholder='indoors or outdoors'
+                        onChange={(e) => this.handleChange(e)}
+                    />
+                    <input
+                        type='text'
+                        name='travel'
+                        placeholder='travel or stay at home'
+                        onChange={(e) => this.handleChange(e)}
+                    />
+                    <input
+                        type='text'
+                        name='food'
+                        placeholder='tops or subway'
+                        onChange={(e) => this.handleChange(e)}
+                    />
+                    <input
+                        type='text'
+                        name='dogOrCat'
+                        placeholder='dog or cat'
+                        onChange={(e) => this.handleChange(e)}
+                    />
+                    <input type='submit' value='submit' />
+                </form>
             </div>
         )
     }
